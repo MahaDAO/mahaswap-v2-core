608e94fe7b9083be5e563837df5ebbba215eeb3c
Yash Agrawal <yashagrawal18@outlook.com>
2021-02-02 16:48:53 +0530

    Add a default factory

--- contracts/ArthswapV2Factory.sol
+++ contracts/ArthswapV2Factory.sol
@@ -20,6 +20,9 @@ contract ArthswapV2Factory is IUniswapV2Factory, Ownable {
     // Who can set the feeTo.
     address public feeToSetter;
 
+    // Default uniswap factory for pairs that are not arth & dai.
+    IUniswapV2Factory defaultFactory;
+
     // Token which will be used for rewards.
     ICustomERC20 public rewardToken;
     // Token which will charge penalty.
@@ -35,7 +38,7 @@ contract ArthswapV2Factory is IUniswapV2Factory, Ownable {
     address arthTokenAddress = address(0x0E3cC2c4FB9252d17d07C67135E48536071735D9);
 
     address[] public allPairs;
-    mapping(address => mapping(address => address)) public getPair;
+    mapping(address => mapping(address => address)) public _getPair;
 
     /**
      * Event.
@@ -46,12 +49,15 @@ contract ArthswapV2Factory is IUniswapV2Factory, Ownable {
      * Constructor.
      */
     constructor(
+        address _defaultFactory,
         address _feeToSetter,
         address _penaltyToken,
         address _rewardToken,
         address _gmuOracle,
         address _uniswapOracle
     ) public {
+        defaultFactory = IUniswapV2Factory(_defaultFactory);
+
         feeToSetter = _feeToSetter;
 
         rewardToken = ICustomERC20(_rewardToken);
@@ -59,6 +65,32 @@ contract ArthswapV2Factory is IUniswapV2Factory, Ownable {
 
         gmuOracle = ISimpleOracle(_gmuOracle);
         uniswapOracle = IUniswapOracle(_uniswapOracle);
+    }
+
+    /**
+     * Getter.
+     */
+    function allPairsLength() external view returns (uint256) {
+        return allPairs.length;
+    }
+
+    function _getPairMode(address token0, address token1) private view returns (uint256) {
+        if (
+            (token0 == arthTokenAddress && token1 == daiTokenAddress) ||
+            (token0 == daiTokenAddress && token1 == arthTokenAddress)
+        ) {
+            return uint256(1);
+        }
+
+        return uint256(0);
+    }
+
+    function getPair(address token0, address token1) public returns (address) {
+        if (_getPair[token0][token1] == address(0)) {
+            return defaultFactory.getPair(token0, token1);
+        }
+
+        return _getPair[token0][token1];
     }
 
     /**
@@ -75,6 +107,12 @@ contract ArthswapV2Factory is IUniswapV2Factory, Ownable {
         require(msg.sender == feeToSetter || msg.sender == owner(), 'UniswapV2: FORBIDDEN');
 
         feeToSetter = _feeToSetter;
+    }
+
+    function setDefaultFactory(address newDefaultFactory) public onlyOwner {
+        require(newDefaultFactory != address(0));
+
+        defaultFactory = newDefaultFactory;
     }
 
     function setGmuOracle(address newGmuOracle) public onlyOwner {
@@ -114,24 +152,6 @@ contract ArthswapV2Factory is IUniswapV2Factory, Ownable {
     }
 
     /**
-     * Getter.
-     */
-    function allPairsLength() external view returns (uint256) {
-        return allPairs.length;
-    }
-
-    function _getPairMode(address token0, address token1) private view returns (uint256) {
-        if (
-            (token0 == arthTokenAddress && token1 == daiTokenAddress) ||
-            (token0 == daiTokenAddress && token1 == arthTokenAddress)
-        ) {
-            return uint256(1);
-        }
-
-        return uint256(0);
-    }
-
-    /**
      * Mutations.
      */
 
@@ -141,7 +161,7 @@ contract ArthswapV2Factory is IUniswapV2Factory, Ownable {
         (address token0, address token1) = tokenA < tokenB ? (tokenA, tokenB) : (tokenB, tokenA);
 
         require(token0 != address(0), 'UniswapV2: ZERO_ADDRESS');
-        require(getPair[token0][token1] == address(0), 'UniswapV2: PAIR_EXISTS'); // single check is sufficient
+        require(_getPair[token0][token1] == address(0), 'UniswapV2: PAIR_EXISTS'); // single check is sufficient
 
         // Check if the tokens form a pair of ARTH/DAI.
         uint256 mode = _getPairMode(tokenA, tokenB);
@@ -177,8 +197,8 @@ contract ArthswapV2Factory is IUniswapV2Factory, Ownable {
             );
         }
 
-        getPair[token0][token1] = pair;
-        getPair[token1][token0] = pair; // populate mapping in the reverse direction
+        _getPair[token0][token1] = pair;
+        _getPair[token1][token0] = pair; // populate mapping in the reverse direction
         allPairs.push(pair);
 
         emit PairCreated(token0, token1, pair, allPairs.length);
