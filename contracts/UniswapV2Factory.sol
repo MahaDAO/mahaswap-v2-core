400c81355cb3d99120fed871e867cc1b98809c68
Yash Agrawal <yashagrawal18@outlook.com>
2021-02-02 13:56:42 +0530

    Modify uniswap factory

--- contracts/UniswapV2Factory.sol
+++ contracts/UniswapV2Factory.sol
@@ -4,6 +4,9 @@ pragma solidity =0.5.16;
 
 import '@openzeppelin/contracts/access/Ownable.sol';
 
+import './interfaces/ICustomERC20.sol';
+import '../interfaces/ISimpleOracle.sol';
+import '../interfaces/IUniswapOracle.sol';
 import './interfaces/IUniswapV2Factory.sol';
 import './UniswapV2Pair.sol';
 
@@ -11,13 +14,35 @@ contract UniswapV2Factory is IUniswapV2Factory, Ownable {
     address public feeTo;
     address public feeToSetter;
 
+    // Token which will charge penalty.
+    ICustomERC20 public penaltyToken;
+    // Token which will be used for rewards.
+    ICustomERC20 public rewardToken;
+
+    // Token which will be used for  to track the latest target price.
+    ISimpleOracle gmuOracle;
+    // Used to track the latest twap price.
+    IUniswapOracle uniswapOracle;
+
     mapping(address => mapping(address => address)) public getPair;
     address[] public allPairs;
 
     event PairCreated(address indexed token0, address indexed token1, address pair, uint256);
 
-    constructor(address _feeToSetter) public {
+    constructor(
+        address _feeToSetter,
+        address _penaltyToken,
+        address _rewardToken,
+        address _gmuOracle,
+        address _uniswapOracle
+    ) public {
         feeToSetter = _feeToSetter;
+
+        penaltyToken = ICustomERC20(_penaltyToken);
+        rewardToken = ICustomERC20(_rewardToken);
+
+        gmuOracle = ISimpleOracle(_gmuOracle);
+        uniswapOracle = IUniswapOracle(_uniswapOracle);
     }
 
     function allPairsLength() external view returns (uint256) {
@@ -34,7 +59,16 @@ contract UniswapV2Factory is IUniswapV2Factory, Ownable {
         assembly {
             pair := create2(0, add(bytecode, 32), mload(bytecode), salt)
         }
-        IUniswapV2Pair(pair).initialize(token0, token1);
+
+        IUniswapV2Pair(pair).initialize(
+            token0,
+            token1,
+            address(penaltyToken),
+            address(rewardToken),
+            address(gmuOracle),
+            address(uniswapOracle)
+        );
+
         getPair[token0][token1] = pair;
         getPair[token1][token0] = pair; // populate mapping in the reverse direction
         allPairs.push(pair);
