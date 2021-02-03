ba66de4a5305d11b9b5ce84e67fce38f6daf5585
Yash Agrawal <yashagrawal18@outlook.com>
2021-02-03 11:19:51 +0530

    Minor changes

--- contracts/interfaces/ICustomERC20.sol
+++ contracts/interfaces/ICustomERC20.sol
@@ -1,12 +1,10 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.6.12;
+pragma solidity ^0.7.4;
 
 import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
 import '@openzeppelin/contracts/token/ERC20/SafeERC20.sol';
 
 interface ICustomERC20 is IERC20 {
-    using SafeERC20 for IERC20;
-
     function burnFrom(address account, uint256 amount) external;
 }
--- contracts/interfaces/ISimpleOracle.sol
+++ contracts/interfaces/ISimpleOracle.sol
@@ -1,8 +1,7 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.6.12;
+pragma solidity ^0.7.4;
 
 interface ISimpleOracle {
     function getPrice() external view returns (uint256 amountOut);
-    // function getReserves() external view returns (uint112 reserve0, uint112 reserve1, uint32 blockTimestamp);
 }
