fafad6b1873d97cb2dd9a2ac69573c71705bef72
Yash Agrawal <yashagrawal18@outlook.com>
2021-02-03 13:06:01 +0530

    Minor changes

--- contracts/interfaces/ICustomERC20.sol
+++ contracts/interfaces/ICustomERC20.sol
@@ -3,7 +3,6 @@
 pragma solidity ^0.7.4;
 
 import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
-import '@openzeppelin/contracts/token/ERC20/SafeERC20.sol';
 
 interface ICustomERC20 is IERC20 {
     function burnFrom(address account, uint256 amount) external;
