e3f50af2caa1677cbb96ceeff68787bb8e90101c
Yash Agrawal <yashagrawal18@outlook.com>
2021-02-04 13:16:50 +0530

    Fix compile issues

--- contracts/IncentiveController.sol
+++ contracts/IncentiveController.sol
@@ -2,6 +2,7 @@
 
 pragma solidity >=0.6.0 <0.8.0;
 
+import '@openzeppelin/contracts/math/SafeMath.sol';
 import '@openzeppelin/contracts/access/Ownable.sol';
 
 import './libraries/Math.sol';
@@ -23,26 +24,26 @@ contract IncentiveController is Ownable {
      */
 
     // Token which will be used to charge penalty or reward incentives.
-    ICustomERC20 token;
+    ICustomERC20 public token;
 
     // Factory that will be using this contract.
-    address factoryAddress;
+    address public factoryAddress;
     // Token which is the main token of a protocol.
-    address protocolTokenAddress;
+    address public protocolTokenAddress;
 
     // Used to track targetPrice.
-    ISimpleOracle gmuOracle;
-    
+    ISimpleOracle public gmuOracle;
+
     // Used to track the latest twap price.
-    IUniswapOracle uniswapOracle;
+    IUniswapOracle public uniswapOracle;
 
     // Default price of when reward is to be given.
-    uint256 rewardPrice = uint256(120).mul(1e16); // ~1.2$
+    uint256 public rewardPrice = uint256(120).mul(1e16); // ~1.2$
     // Default price of when penalty is to be charged.
-    uint256 penaltyPrice = uint256(95).mul(1e16); // ~0.95$
+    uint256 public penaltyPrice = uint256(95).mul(1e16); // ~0.95$
 
     // Should we use oracle to get diff. price feeds or not.
-    bool useOracle = false;
+    bool public useOracle = false;
 
     // Max. reward per hour to be given out.
     uint256 public mahaRewardPerHour = 13 * 1e18;
@@ -52,7 +53,7 @@ contract IncentiveController is Ownable {
     /**
      * Modifiers
      */
-`
+
     modifier onlyFactory {
         require(msg.sender == factoryAddress, 'Controller: Forbidden');
 
@@ -63,7 +64,7 @@ contract IncentiveController is Ownable {
      * Constructor.
      */
     constructor(address _factoryAddress, address _protocolTokenAddress) {
-        factoryAddress = _factoryAddress
+        factoryAddress = _factoryAddress;
 
         protocolTokenAddress = _protocolTokenAddress;
     }
@@ -84,7 +85,7 @@ contract IncentiveController is Ownable {
         }
     }
 
-    function getPenaltyPrice() view returns (uint256) {
+    function getPenaltyPrice() public view returns (uint256) {
         // If (useOracle) then get penalty price from an oracle
         // else get from a variable.
         // This variable is settable from the factory.
@@ -93,7 +94,7 @@ contract IncentiveController is Ownable {
         return _getCashPrice();
     }
 
-    function getRewardIncentivePrice() view returns (uint256) {
+    function getRewardIncentivePrice() public view returns (uint256) {
         // If (useOracle) then get reward price from an oracle
         // else get from a variable.
         // This variable is settable from the factory.
@@ -138,16 +139,22 @@ contract IncentiveController is Ownable {
      * Mutations.
      */
 
-    function _checkAndPenalize(uint256 price, uint256 amountOutA, uint256 amountOutB, bool isTokenAProtocolToken) private {
+    function _checkAndPenalize(
+        uint256 price,
+        uint256 amountOutA,
+        uint256 amountOutB,
+        bool isTokenAProtocolToken,
+        address from
+    ) private {
         // The token which is the main protocol token and we are selling, then the other token should have
         // out amount > 0.
-        require(isTokenAProtocolToken ? amountOutB > 0 : amountOutA >  0, 'Controller: invalid operation');
-        
+        require(isTokenAProtocolToken ? amountOutB > 0 : amountOutA > 0, 'Controller: invalid operation');
+
         // Get the penalty price
-        uint256 penaltyPrice = getPenaltyPrice();
+        uint256 penaltyTriggerPrice = getPenaltyPrice();
 
         // Check if we are below the penaltyPrice.
-        if (price < penaltyPrice) {
+        if (price < penaltyTriggerPrice) {
             // If penalty is on then we penalize
 
             uint256 amountToBurn = 0;
@@ -160,13 +167,15 @@ contract IncentiveController is Ownable {
                 // If any is 0, then we figure out the amount as per price.
 
                 // If A is protocolToken, then amountOutB can not be 0 and vice versa.
-                // However if amountOutProtocol is 0, 
+                // However if amountOutProtocol is 0,
                 // then we calculate the amount being sold as per price and amount of other token swapped.
                 // Lets say A = 2$ and B = 1$, then A/B = 2/1 = 2.
                 // Hence A = 2B.
                 // Hence if we are selling A and outAmount is 0,
                 // then we can calculate it with 2 * outAmountB.
-                amountToBurn = penaltyPrice.sub(price).mul(isTokenAProtocolToken ? amountOutB : amountOutA).div(100);
+                amountToBurn = (
+                    penaltyTriggerPrice.sub(price).mul(isTokenAProtocolToken ? amountOutB : amountOutA).div(100)
+                );
             }
 
             if (amountToBurn > 0) {
@@ -179,18 +188,24 @@ contract IncentiveController is Ownable {
         }
     }
 
-    function _checkAndIncentivize(address to, uint256 price, uint256 amountOutA, uint256 amountOutB, bool isTokenAProtocolToken) private {
+    function _checkAndIncentivize(
+        address to,
+        uint256 price,
+        uint256 amountOutA,
+        uint256 amountOutB,
+        bool isTokenAProtocolToken
+    ) private {
         // The token which is the main protocol token and we are buying hence that token should have out amount > 0.
         require(isTokenAProtocolToken ? amountOutA > 0 : amountOutB > 0, 'Controller: invalid operation');
 
         // Check if we are above the reward price.
         // NOTE: can this be changed to price > getPenaltyPrice()?
-        if (price > getRewardPrice()) {
+        if (price > getRewardIncentivePrice()) {
             // If reward is on then we reward.
 
             // Based on volumne of the tx & hourly rate, figure out the amount to reward.
             uint256 rate = token.balanceOf(address(this)).div(30).div(24); // Calculate the rate for curr. period.
-            
+
             uint256 amountToReward = 0;
 
             // Check if any amount is 0 or not.
@@ -201,18 +216,15 @@ contract IncentiveController is Ownable {
                 // If any is 0, then we figure out which one is 0.
 
                 // If A is protocolToken, then amountOutA can not be 0 and vice versa.
-                // However if the other token out amount is 0, 
+                // However if the other token out amount is 0,
                 // then we calculate the amount being sold as per price and amount of the protocolToken.
                 // Refer Line 165 to 168.
-                amountToBurn = rate.mul(price.mul(isTokenAProtocolToken ? amountOutA : amountOutA));
+                amountToReward = rate.mul(price.mul(isTokenAProtocolToken ? amountOutA : amountOutA));
             }
-            
-            // Calculate the amount as per volumne and rate. 
+
+            // Calculate the amount as per volumne and rate.
             // Cap the amount to a maximum rewardPerHour if amount > maxRewardPerHour.
-            amountToReward = Math.min(
-                rate.mul(amountToReward),
-                mahaRewardPerHour
-            )
+            amountToReward = Math.min(rate.mul(amountToReward), mahaRewardPerHour);
 
             if (amountToReward > 0) {
                 // Send reward to the appropriate address.
@@ -230,40 +242,38 @@ contract IncentiveController is Ownable {
     function conductChecks(
         address tokenA,
         address tokenB,
-        uint256 reserveA,
-        uint256 reserveB,
-        uint256 newReserveA,
-        uint256 newReserveB,
+        uint112 reserveA,
+        uint112 reserveB,
+        uint112 newReserveA,
+        uint112 newReserveB,
         address from,
         address to,
         uint256 amountOutA,
         uint256 amountOutB
     ) public virtual onlyOwner {
-        require(tokenA == penaltyTokenAddress || tokenB == penaltyTokenAddress, 'Controller: invalid config');
+        require(tokenA == protocolTokenAddress || tokenB == protocolTokenAddress, 'Controller: invalid config');
 
-        bool isTokenAProtocolToken = tokenA == penaltyTokenAddress;
+        bool isTokenAProtocolToken = tokenA == protocolTokenAddress;
 
         // Get the price for the token.
-        uint256 price = isTokenAProtocolToken 
-            ? uint256(UQ112x112.encode(reserveA).uqdiv(reserveB)) 
-            : uint256(UQ112x112.encode(reserveB).uqdiv(reserveA));
+        uint256 price =
+            isTokenAProtocolToken
+                ? uint256(UQ112x112.encode(reserveA).uqdiv(reserveB))
+                : uint256(UQ112x112.encode(reserveB).uqdiv(reserveA));
 
         // Check if we are below the targetPrice.
         if (price < _getTargetPrice()) {
             // If we are below the target price, then we penalize or incentivize the tx sender.
 
-            bool isBuying = false;
-
             // Check if we are buying or selling.
             if (
-                (isTokenAProtocolToken && newReserveA > reserveA) ||
-                (!isTokenAProtocolToken && newReserveB > reserveB) 
+                (isTokenAProtocolToken && newReserveA > reserveA) || (!isTokenAProtocolToken && newReserveB > reserveB)
             ) {
                 // If we are buying the main protocol token, then we incentivize the tx sender.
-                 _checkAndIncentivize(to, price, amountOutA, amountOutB, isTokenAProtocolToken);
+                _checkAndIncentivize(to, price, amountOutA, amountOutB, isTokenAProtocolToken);
             } else {
                 // Else we penalize the tx sender.
-                _checkAndPenalize(price, amountOutA, amountOutB, isTokenAProtocolToken);
+                _checkAndPenalize(price, amountOutA, amountOutB, isTokenAProtocolToken, from);
             }
         }
     }
