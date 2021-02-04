1a2991560f1a65df45a71855ee14070c89939b11
Yash Agrawal <yashagrawal18@outlook.com>
2021-02-04 13:42:50 +0530

    Minor changes

--- contracts/test/ERC20.sol
+++ contracts/test/ERC20.sol
@@ -2,10 +2,10 @@
 
 pragma solidity >=0.6.0 <0.8.0;
 
-import '../UniswapV2ERC20.sol';
+import '../ArthswapV1ERC20.sol';
 
-contract ERC20 is UniswapV2ERC20 {
-    constructor(uint256 _totalSupply) public {
+contract ERC20 is ArthswapV1ERC20 {
+    constructor(uint256 _totalSupply) {
         _mint(msg.sender, _totalSupply);
     }
 }
