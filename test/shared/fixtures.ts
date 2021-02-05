318ff014f495a733454e454b665038f78e9530e4
Yash Agrawal <yashagrawal18@outlook.com>
2021-02-05 11:12:11 +0530

    Add basic test for controller

--- test/shared/fixtures.ts
+++ test/shared/fixtures.ts
@@ -7,6 +7,8 @@ import { expandTo18Decimals } from './utilities'
 import ERC20 from '../../build/ERC20.json'
 import UniswapV2Factory from '../../build/ArthswapV1Factory.json'
 import UniswapV2Pair from '../../build/ArthswapV1Pair.json'
+import ArthIncentiveController from '../../build/ArthIncentiveController.json'
+
 
 interface FactoryFixture {
   factory: Contract
@@ -42,4 +44,38 @@ export async function pairFixture(provider: Web3Provider, [wallet]: Wallet[]): P
   const token1 = tokenA.address === token0Address ? tokenB : tokenA
 
   return { factory, token0, token1, pair }
+}
+
+
+interface ControllerFixture extends FactoryFixture {
+  token0: Contract
+  token1: Contract
+  pair: Contract
+  controller: Contract
+}
+
+export async function controllerFixture(provider: Web3Provider, [wallet]: Wallet[]): Promise<ControllerFixture> {
+  const { factory } = await factoryFixture(provider, [wallet])
+
+  const tokenA = await deployContract(wallet, ERC20, [expandTo18Decimals(10000)], overrides)
+  const tokenB = await deployContract(wallet, ERC20, [expandTo18Decimals(10000)], overrides)
+
+  const protocolToken = await deployContract(wallet, ERC20, [expandTo18Decimals(10000)], overrides)
+
+  await factory.createPair(tokenA.address, tokenB.address, overrides)
+  const pairAddress = await factory.getPair(tokenA.address, tokenB.address)
+  const pair = new Contract(pairAddress, JSON.stringify(UniswapV2Pair.abi), provider).connect(wallet)
+
+  const controller = await deployContract(
+    wallet,
+    ArthIncentiveController,
+    [pairAddress, protocolToken.address, Math.floor(Date.now() / 1000)],
+    overrides
+  )
+
+  const token0Address = (await pair.token0()).address
+  const token0 = tokenA.address === token0Address ? tokenA : tokenB
+  const token1 = tokenA.address === token0Address ? tokenB : tokenA
+
+  return { factory, token0, token1, pair, controller }
 }
