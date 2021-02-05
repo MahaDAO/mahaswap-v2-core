6d39de8789467810c92735e48531a248fb1b7a6d
Yash Agrawal <yashagrawal18@outlook.com>
2021-02-05 18:17:54 +0530

    Minor changes

--- contracts/test/MockBurnableERC20.sol
+++ contracts/test/MockBurnableERC20.sol
@@ -38,7 +38,7 @@ contract MockBurnableERC20 is IUniswapV2ERC20 {
             )
         );
 
-        _mint(msg.sender, _totalSupply.mul(10000000000));
+        _mint(msg.sender, _totalSupply);
     }
 
     // Just for testing purposes.
