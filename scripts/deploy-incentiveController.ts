dc66d98bd2019313e9802346d2c5694d981e3e86
Yash Agrawal <yashagrawal18@outlook.com>
2021-02-08 21:40:28 +0530

    Fix the protocol(target) token address in gorli deployment script

--- scripts/deploy-incentiveController.ts
+++ scripts/deploy-incentiveController.ts
@@ -20,7 +20,7 @@ async function main() {
   // Deploy new treasury.
   const params = [
     '0x38eBdcB14674Ea32c982d7c93CcDf28F06a5c7a6', // pair address
-    '0xfa1c36f01fea68726ec60cf1f050783aa9d1076c', // target token (ARTH)
+    '0x7e53072c6Ca9104C60362586D9080a22eA366E91', // target token (ARTH)
     '0x981f0d72650583d4d6961def6c186d84d9e0f2b9', // incentive token (NAHA)
     "13000000000000000000", // rewardPerHour
     "87000000000000000" // ARTH to MAHA price
