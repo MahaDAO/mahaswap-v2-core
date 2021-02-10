48685fc9cb667409b8b4fe00a5984221df985684
Steven Enamakel <enamakel@vezures.com>
2021-02-10 17:42:33 +0300

    added logs for other chains

--- scripts/deploy-incentiveController.ts
+++ scripts/deploy-incentiveController.ts
@@ -18,13 +18,32 @@ async function main() {
 
 
   // Deploy new treasury.
-  const params = [
-    '0x38eBdcB14674Ea32c982d7c93CcDf28F06a5c7a6', // pair address
-    '0x7e53072c6ca9104c60362586d9080a22ea366e91', // target token (ARTH)
+  const paramsMainnet = [
+    '0x1C36D9E60cac6893652b74E357f3829A0f5095e0', // pair address
+    '0x0e3cc2c4fb9252d17d07c67135e48536071735d9', // target token (ARTH)
     '0xb4d930279552397bba2ee473229f89ec245bc365', // incentive token (NAHA)
-    "250000000000000000000", // rewardPerHour
+    "500000000000000000000", // rewardPerHour
     "61538461540000000" // ARTH to MAHA price
   ]
+
+  const paramsRopsten = [
+    '0x8c85541cc02e88242cb706f88f0724ea874dfb0e', // pair address
+    '0x5ac2a32bfa475765558cea2a0fe0bf0207d58ca4', // target token (ARTH)
+    '0xcd24efb0f7285cb923cab11a85fbdb1523f10011', // incentive token (NAHA)
+    "250000000000000000000", // rewardPerHour
+    "1000000000000000000" // ARTH to MAHA price
+  ]
+
+  const paramsGoreli = [
+    '0x38eBdcB14674Ea32c982d7c93CcDf28F06a5c7a6', // pair address
+    '0xfa1c36f01fea68726ec60cf1f050783aa9d1076c', // target token (ARTH)
+    '0x981f0d72650583d4d6961def6c186d84d9e0f2b9', // incentive token (NAHA)
+    "250000000000000000000", // rewardPerHour
+    "1000000000000000000" // ARTH to MAHA price
+  ]
+
+
+  const params = paramsMainnet
 
   console.log(params)
   const factory = await contract.connect(operator).deploy(...params);
--- scripts/verify-incentivecontroller.js
+++ scripts/verify-incentivecontroller.js
@@ -1,10 +1,9 @@
 module.exports = [
-  '0x38eBdcB14674Ea32c982d7c93CcDf28F06a5c7a6', // pair address
-  '0x7e53072c6ca9104c60362586d9080a22ea366e91', // target token (ARTH)
-  '0x981f0d72650583d4d6961def6c186d84d9e0f2b9', // incentive token (NAHA)
-  "250000000000000000000", // rewardPerHour
-  "87000000000000000" // ARTH to MAHA price
+  '0x1C36D9E60cac6893652b74E357f3829A0f5095e0',
+  '0x0e3cc2c4fb9252d17d07c67135e48536071735d9',
+  '0xb4d930279552397bba2ee473229f89ec245bc365',
+  '500000000000000000000',
+  '61538461540000000'
 ]
-
-// npx hardhat verify --constructor-args scripts/verify-incentivecontroller.js 0x3E617c60C093881ef221BCA1216266f14B60604c --network goerli
+// npx hardhat verify --constructor-args scripts/verify-incentivecontroller.js 0xB47C55930D705f28eBF98A6d637eddA1aB98924D --network mainnet
 // 10000003099989911203319/10000008908138970689300
