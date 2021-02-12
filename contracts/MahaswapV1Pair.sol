bedeff8289a41f4b196870c9d9aeda252f89ba12
Yash Agrawal <yashagrawal18@outlook.com>
2021-02-12 22:46:51 +0530

    Call controller after updating state of pair

--- contracts/MahaswapV1Pair.sol
+++ contracts/MahaswapV1Pair.sol
@@ -265,9 +265,10 @@ contract MahaswapV1Pair is IMahaswapV1Pair, MahaswapV1ERC20, Ownable {
             );
         }
 
+        _update(balance0, balance1, _reserve0, _reserve1);
+
         // Get reserves after the trade was made.
         _reward(amount0Out, amount1Out, amount0In, amount1In, to);
-        _update(balance0, balance1, _reserve0, _reserve1);
 
         emit Swap(msg.sender, amount0In, amount1In, amount0Out, amount1Out, to);
     }
