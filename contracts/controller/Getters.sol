15335108368103f30c4ef68c9b17855ba474288d
Steven Enamakel <enamakel@vezures.com>
2021-03-14 11:55:19 +0300

    removed some fn's from the getters

--- contracts/controller/Getters.sol
+++ contracts/controller/Getters.sol
@@ -8,22 +8,6 @@ import {State} from './State.sol';
  * NOTE: Contract MahaswapV1Pair should be the owner of this controller.
  */
 contract Getters is State {
-    // Given an output amount of an asset and pair reserves,
-    // Returns a required input amount of the other asset.
-    function _getAmountIn(
-        uint256 amountOut,
-        uint256 reserveIn,
-        uint256 reserveOut
-    ) private pure returns (uint256 amountIn) {
-        require(amountOut > 0, 'Controller: INSUFFICIENT_OUTPUT_AMOUNT');
-        require(reserveIn > 0 && reserveOut > 0, 'Controller: INSUFFICIENT_LIQUIDITY');
-
-        uint256 numerator = reserveIn.mul(amountOut).mul(1000);
-        uint256 denominator = reserveOut.sub(amountOut).mul(997);
-
-        amountIn = (numerator / denominator).add(1);
-    }
-
     function getLatestQuoteInUSD() public view returns (uint256) {
         if (address(quotePriceFeed) == address(0)) return 1e18;
 
